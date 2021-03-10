9e35a763397484cac99148bf60a4b9262f1e71e7
maidersonn <maidersonn@gmail.com>
2021-03-10 17:57:07 +0100

    gets repositories from github api

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3300;
-const path = require("path");
 const router = express.Router();
-let mustacheExpress = require("mustache-express");
+const mustacheExpress = require("mustache-express");
+const https = require("https");
+const { title } = require("process");
 
 app.engine("html", mustacheExpress());
 app.set("view engine", "html");
@@ -17,7 +18,39 @@ router.get("/", (req, res) => {
 });
 
 router.get("/projects", (req, res) => {
-  res.render("layout", { title: "Projects", projects: true });
+  const options = {
+    headers: {
+      Authorization: "token b89cff9ad6567d7eb05b6a39386c4e46e7275ef0",
+      "user-agent": "node.js",
+    },
+  };
+  https.get(
+    "https://api.github.com/users/maidersonn/repos",
+    options,
+    (jsonres) => {
+      let data = "";
+
+      jsonres.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      jsonres.on("close", () => {
+        const repositories = JSON.parse(data);
+
+        const repositoriesToLayout = repositories.filter(
+          (repository) =>
+            repository.name !== "bikoteak" &&
+            repository.name !== "Curso-de-JavaScript_ed15"
+        );
+
+        res.render("layout", {
+          title: "Projects",
+          projects: repositoriesToLayout,
+          projectPartial: true,
+        });
+      });
+    }
+  );
 });
 
 router.get("/contact", (req, res) => {
